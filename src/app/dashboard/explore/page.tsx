7db7ad8dab09d20a03138a2ec5a2b1e837410f60
Sahil-tried-coding <sahiltriedcoding@gmail.com>
2025-05-06 14:53:24 +0530

    making the product responsive

--- src/app/dashboard/explore/page.tsx
+++ src/app/dashboard/explore/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { techCareers } from './carrerDB'
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const Explore = () => {
+  const [career, setCareer] = useState('')
+  console.log(career)
   return (
     <div className=''>
       <div className='flex items-center gap'>
@@ -11,13 +15,19 @@ const Explore = () => {
       <h1 className='mt-4  capitalize font-semibold text-2xl text-center whitespace-nowrap'>Confused About Career options and what skills required? here's the breakdown to learn</h1>
 
       </div>
-
-      <Input type='text' placeholder='Search Here' />
+      {/* career.toLowerCase() == item.title.toLowerCase() */}
+      <div className='flex gap-2'>
+      <Input onChange={(e)=>setCareer(e.target.value)} value={career} type='text' placeholder='Search Here' />
+      {/* <Button variant={"outline"}>Search </Button> */}
+      </div>
             {
         techCareers.map((item,index)=>(
           <div className='pl-12 flex flex-col gap-1.5 mt-4 border-b pb-4.5' key={index}>
 
-            <h1 className='font-semibold text-xl text-blue-500'>{item.title}</h1>
+
+  <h1 className='font-semibold text-xl text-blue-500'>{item.title}</h1>
+
+            
             <h1 className='text-sm'>{item.description}</h1>
             <div className='flex gap-2.5 flex-wrap'>
             {
